7e306d17b2b21d5cb40c5be1638b1a512a2a1228
hmillar123 <163072503+hmillar123@users.noreply.github.com>
2024-03-20 10:08:57 -0700

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,8 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Hannah Millar',
+
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
